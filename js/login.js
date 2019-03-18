992f6d86ebdd03d1c426c8a305976daeaedeb293
mike4747 <mike4747@oh.rr.com>
2019-03-17 22:05:02 -0400

    added email login

--- js/login.js
+++ js/login.js
@@ -21,7 +21,8 @@ var uiConfig = {
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ]
   };
 
