680aa794e2b479075e71066ba97b8844b5aca207
mike4747 <mike4747@oh.rr.com>
2019-03-20 22:48:21 -0400

    a few minor updates to several files

--- js/parks.js
+++ js/parks.js
@@ -9,11 +9,11 @@ function parkData(data) {//data in imperial
     for (var trailIndex = 0; trailIndex < trails.length; trailIndex++) {
         var tdata = trails[trailIndex];//Trail data object handle from hiking project API
         var newDiv = $("<div class='clearfix border-bottom'>");
-        newDiv.append("<p><a href='" + tdata.url + "' target='_blank'>" + tdata.name + "</a></p>");
+        newDiv.append("<p class='my-2'><a href='" + tdata.url + "' target='_blank'>" + tdata.name + "</a></p>");
         newDiv.append("<img src='" + tdata.imgSqSmall + "' alt='" + tdata.name + "' class='float-left mr-2 mb-1'>");
-        newDiv.append("<p class='small'><b>Length: </b>" + tdata.length + " miles</p>");
-        newDiv.append("<p class='small'>" + tdata.stars + " stars (" + tdata.starVotes + " reviews)</p>");
-        newDiv.append("<p class='small'><b>Loc:</b> " + tdata.location + "</p>");
+        newDiv.append("<p class='small my-2'><b>Length: </b>" + tdata.length + " miles</p>");
+        newDiv.append("<p class='small my-2'>" + tdata.stars + " stars (" + tdata.starVotes + " reviews)</p>");
+        newDiv.append("<p class='small my-2'><b>Loc:</b> " + tdata.location + "</p>");
         $("#parks_card").append(newDiv);
     }
 }
